dc7127f539d60004907b82ca7c9db44bfca22e00
srajpal <srajpal@iu.edu>
2020-03-24 19:39:35 -0400

    Created our first api view

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -7,20 +7,20 @@ from django.contrib.auth.models import BaseUserManager
 
 class UserProfileManager( BaseUserManager ) :
     """Helps Django work with our custom user manager."""
-    def create_user( self , email , name , password = None ) :
+    def create_user( self , email , name , password ) :
         """Create a new user profile object."""
         if not email :
             raise ValueError( 'Users must have an email address.' )
 
         email = self.normalize_email( email )
-        user = self.model( email = email , name = name )
+        user = self.model( email = email , name = name , )
 
         user.set_password( password )
         user.save( using = self._db )
 
         return user
 
-    def create_super_user( self , email , name , password ) :
+    def create_superuser( self , email , name , password ) :
         """Creates and saves a new superuser with given details."""
         user = self.create_user( email , name , password )
 
@@ -40,7 +40,7 @@ class UserProfile( AbstractBaseUser , PermissionsMixin ) :
     objects = UserProfileManager( )
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = [ 'name' ]
+    REQUIRED_FIELD = [ 'name', ]
 
     def get_full_name( self ) :
         """Used to get a user's full name."""
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class HelloApiView( APIView ) :
+    """Test API View."""
+
+    def get( self , request , format = None ) :
+        """Return a list of API features."""
+        an_apiview = [
+            'Uses HTTP method as functions (get, post, patch, put, delete)' ,
+            'Is similar to a traditional Django View' ,
+            'Gives you most control over logic' ,
+            'Is mapped manually to URLs'
+        ]
+
+        return Response( { 'message' : 'hello!' , 'an_apiview' : an_apiview } )
